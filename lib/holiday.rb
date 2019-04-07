ebb52172de42d194db3190423e7432772b26b15f
Renee Deka Ambia <dekadekadeka@gmail.com>
2019-04-07 21:08:44 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,7 +72,7 @@ def all_supplies_in_holidays(holiday_hash)
 holiday_hash.each do |season, holiday|
   puts "#{season.capitalize}:"
   holiday.each do |day, supplies|
-    puts "  #{day}: #{supplies.join(", ").capitalize}"
+    puts "  #{day.capitalize}: #{supplies.join(", ").capitalize}"
   end
 end
 end
