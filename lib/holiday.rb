1014b5a1c421eb5d5fd3bb96f88b522233b72941
Renee Deka Ambia <dekadekadeka@gmail.com>
2019-04-07 21:19:24 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -80,9 +80,8 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-bbqdays = []
 holiday_hash.map do |season, holiday|
-  holiday.each do |day, supplies|
+  holiday.map do |day, supplies|
     if supplies.include?("BBQ")
       bbqdays << day
     end
