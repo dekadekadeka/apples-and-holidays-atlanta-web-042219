84f177f4bf73c1b5e39f0b4c4b88564224ebbe2c
Renee Deka Ambia <dekadekadeka@gmail.com>
2019-04-07 21:20:41 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -82,8 +82,7 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
 holiday_hash.map do |season, holiday|
   holiday.map do |day, supplies|
-    if supplies.include?("BBQ")
-      day
+    holiday if supplies.include?("BBQ")
     end
   end.flatten.compact
 end
